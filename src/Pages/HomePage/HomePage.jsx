52f684781e97ea400eaf69fc8fd0e541d0379b93
devRezaulKarim <dev.rezaulkarim@gmail.com>
2023-12-03 19:26:24 +0600

    home page done

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,28 @@
+import { Link } from "react-router-dom";
 import "./HomePage.css";
+import heroImg from "../../assets/problem-solving.gif";
 
 export default function HomePage() {
   return (
     <>
       <div className="homePageContainer">
-        <h1>HomePage</h1>
+        <div className="heroLeft">
+          <h1>
+            Lorem <span>ipsum</span> <span>dolor</span> sit amet.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+            nesciunt doloremque distinctio expedita saepe ipsam fuga nisi,
+            incidunt delectus! Ducimus culpa eaque rem unde, sint dolorum
+            numquam repellat natus similique.
+          </p>
+          <div>
+            <Link>lorem</Link>
+          </div>
+        </div>
+        <div className="heroRight">
+          <img src={heroImg} alt="" />
+        </div>
       </div>
     </>
   );
